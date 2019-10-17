0b315c47c76ded5bbd4e253650459ee3e6e9c6d6
鸡毛 <shihao.gu@perfma.com>
2019-10-17 13:03:59 +0800

    查询接口完成

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import * as BRCore from 'blockrpg-core';
+import MapBlockController from './Module/MapBlock/Controller';
 
 const app = new BRCore.Koa.App((app) => {
-  return app;
+  return app.use(MapBlockController);
 }, true);
 
 app.Listen(3001);
