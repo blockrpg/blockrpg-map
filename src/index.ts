0c1a52aa4f95669f9c0c9310b2de14553b5cb64b
鸡毛 <shihao.gu@perfma.com>
2019-10-18 13:10:57 +0800

    跟进框架

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import Router from 'koa-router';
 import * as BRCore from 'blockrpg-core';
 import MapBlockController from './Module/MapBlock/Controller';
 
-const app = new BRCore.Koa.App((app) => {
+const app = new BRCore.Koa.App('Map', (app) => {
   return app.use(MapBlockController);
 }, true);
 
-app.Listen(3002);
+app.Listen();
